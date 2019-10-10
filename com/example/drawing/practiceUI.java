abff22b2d25549efe023a09aed0e0636fb3d5c5b
Kharee1 <keo6@njit.edu>
2019-10-10 13:45:54 -0400

    Adding file system asssignment

---
+++ com/example/drawing/practiceUI.java
@@ -0,0 +1,13 @@
+package com.example.drawing;
+
+import javax.swing.JFrame;
+
+public class practiceUI {
+	public static void main(String[] args) {
+		JFrame = frame new JFrame("Having Fun?");
+		
+		JPanel simpleChat = new JPanel;
+		
+	}
+
+}
