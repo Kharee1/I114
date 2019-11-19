db6139d8143516e74f5fe6e33dc3b3fc61547d86
Kharee1 <keo6@njit.edu>
2019-11-19 14:20:15 -0500

    ADding logic to check if file is on system

--- wordHunt/WordHunt.java
+++ wordHunt/WordHunt.java
@@ -1,11 +1,16 @@
 package wordHunt;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Random;
+import java.util.Scanner;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -14,27 +19,52 @@ public class WordHunt{
 	public static final int nROWS = 10;
 	public static final int nCOLS = 10;
 	
+	public void fileFinder(String fileName) {
+		File file = new File(fileName);
+		try(Scanner scan = new Scanner(file)){
+			String gameWords = "";
+			while (scan.hasNextLine()) {
+				String data = scan.nextLine();
+		        gameWords += data;
+		        if(scan.hasNextLine()) {
+		        	gameWords += System.lineSeparator();
+		        }
+			}
+			System.out.println(gameWords);
+		}
+		catch(FileNotFoundException e) {
+			e.printStackTrace();
+			System.out.println("The necessary game file wasn't found");
+		}
+	}
+	
 	
 
 public static void main(String[] args) {
+	
+	String fileName = "gamedata.txt";
+	WordHunt game = new WordHunt();
+	game.fileFinder(fileName);
+	
 	JFrame frame = new JFrame("The Greetings Word Hunt");
 	frame.setLayout(new BorderLayout());
-	frame.setMinimumSize(new Dimension(600,600));
 	frame.setSize(new Dimension(600,600));
-	frame.setVisible(true);
+	frame.setMinimumSize(new Dimension(600,600));
 	
-	
-	
+
 	Dimension gridDimensions = new Dimension(400,400);
 	JPanel grid = new JPanel();
 	grid.setLayout(new GridLayout(nROWS, nCOLS));
 	grid.setSize(gridDimensions);
-	frame.add(grid);
+	grid.setBorder(BorderFactory.createLineBorder(Color.black));
+	
 	
 	Random num = new Random();
 	int i = 0;
 	Dimension buttonSize = new Dimension(10,10);
 	
+
+    
 	for(int y = 0; y < nROWS; y++) {
 		for(int x = 0; x < nCOLS; x++) {
 			JButton bt = new JButton();
@@ -44,7 +74,6 @@ public static void main(String[] args) {
 		    char random = alphabet.charAt(num.nextInt(alphabet.length()));
 		    String randomLetter = String.valueOf(random);
 		    System.out.println(randomLetter);
-			//String buttonText = String.format("Index: %1$s Coord: (%2$s, %3$s)", i, x, y);
 			bt.setText(randomLetter);
 			bt.setLocation(x, y);
 			bt.setSize(buttonSize);
@@ -54,5 +83,11 @@ public static void main(String[] args) {
 	    	} // prints 50 rand
 	
 		}
+	frame.setVisible(true);
+	frame.add(grid);
+	//public static void highlight{}
+	
+	//}
+	
 	}
 }
