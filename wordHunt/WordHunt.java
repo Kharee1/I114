af095a3bb957c5691c3172d96c5a7ce7dcca6ae4
Kharee1 <keo6@njit.edu>
2019-12-03 14:24:10 -0500

    adding string check changes

--- wordHunt/WordHunt.java
+++ wordHunt/WordHunt.java
@@ -2,13 +2,14 @@ package wordHunt;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -18,10 +19,13 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class WordHunt{
+	//creating rows and cols variable
 	public static final int nROWS = 10;
 	public static final int nCOLS = 10;
-	
+	public static final int gridSize = nROWS * nCOLS;
+
 	public boolean fileFinder(String fileName) {
+		
 		File file = new File(fileName);
 		try(Scanner scan = new Scanner(file)){
 			String gameWords = "";
@@ -42,40 +46,39 @@ public class WordHunt{
 		}
 		return true;
 	}
-
+	
+	public void foundWords() {
+		
+	}
 
 public static void main(String[] args) {
 	
 	String fileName = "gamedata.txt";
 	WordHunt game = new WordHunt();
 	if(game.fileFinder(fileName)) {
-		System.out.println("Beginning game shortly");
+		System.out.println("\n" + "Beginning game shortly.");
+		
 		JFrame frame = new JFrame("The Greetings Word Hunt");
 		frame.setLayout(new BorderLayout());
 		frame.setSize(new Dimension(600,600));
 		frame.setMinimumSize(new Dimension(600,600));
 		
-
 		Dimension gridDimensions = new Dimension(400,400);
-		JPanel grid = new JPanel();
-		grid.setLayout(new GridLayout(nROWS, nCOLS));
-		grid.setSize(gridDimensions);
-		grid.setBorder(BorderFactory.createLineBorder(Color.black));
+		JPanel canvas = new JPanel();
+		canvas.setLayout(new GridLayout(nROWS, nCOLS));
+		canvas.setSize(gridDimensions);
+		canvas.setBorder(BorderFactory.createLineBorder(Color.black));
 		
 		JPanel container = new JPanel();
 		container.setSize(new Dimension(800,800));
 		container.setMinimumSize(new Dimension(600, 600));
 		container.setBorder(BorderFactory.createLineBorder(Color.red));
 		
-		JPanel holder = new JPanel();
-		holder.setSize(new Dimension(1000,1000));
-		
+		ArrayList<String> wordCheck = new ArrayList<String>();
 		
 		Random num = new Random();
 		int i = 0;
 		Dimension buttonSize = new Dimension(10,10);
-		
-
 	    
 		for(int y = 0; y < nROWS; y++) {
 			for(int x = 0; x < nCOLS; x++) {
@@ -83,22 +86,30 @@ public static void main(String[] args) {
 			    String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 			    char random = alphabet.charAt(num.nextInt(alphabet.length()));
 			    String randomLetter = String.valueOf(random);
-			    System.out.println(randomLetter);
 				bt.setText(randomLetter);
 				bt.setLocation(x, y);
 				bt.setSize(buttonSize);
+				
+				bt.setBackground(Color.white);
+				bt.addActionListener(new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						
+						((JButton)e.getSource()).setBackground(Color.red);
+						System.out.println(e.getSource());
+						wordCheck.add(((JButton)e.getSource()).getText());
+						System.out.println(wordCheck.toString());
+					}
+				});
 		
-		grid.add(bt);
-		container.add(grid);
+		
+		canvas.add(bt);
+		
 		i++;
-		    	} // prints 50 rand
-		
+		    	}
 			}
 		frame.setVisible(true);
-		frame.add(grid);
-	}
-	
-	
-	
+		frame.add(canvas);
+		}
 	}
 }
