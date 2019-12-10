03d31fd237d72403b3355a8d090d3962996ce0d3
Kharee1 <keo6@njit.edu>
2019-12-10 12:02:01 -0500

    adding pieces to make it cleint server ready

--- wordHunt/WordHunt.java
+++ wordHunt/WordHunt.java
@@ -6,9 +6,14 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
+import java.awt.event.KeyEvent;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
@@ -18,12 +23,15 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class WordHunt{
+public class WordHunt {
 	//creating rows and cols variable
 	public static final int nROWS = 10;
 	public static final int nCOLS = 10;
 	public static final int gridSize = nROWS * nCOLS;
-
+	public ArrayList<String> wordsToFind = new ArrayList<String>();
+	public ArrayList<Character> wordCheck = new ArrayList<Character>();
+	
+	
 	public boolean fileFinder(String fileName) {
 		
 		File file = new File(fileName);
@@ -36,25 +44,114 @@ public class WordHunt{
 		        	gameWords += System.lineSeparator();
 		        }
 			}
-			System.out.println(gameWords);
+			wordsToFind.add(gameWords);
 			scan.close();
+			System.out.println(wordsToFind);
 		
 		}catch(FileNotFoundException e) {
 			e.printStackTrace();
-			System.out.println("The necessary game file wasn't found. Please download the file game file first");
+			System.out.println("The necessary game file wasn't found. Please download the necessary game file first");
 			return false;
 		}
 		return true;
 	}
 	
-	public void foundWords() {
-		
+	public void wordComparison() {
+		for (int i =0; i < wordsToFind.size(); i++) {
+			System.out.println(wordsToFind.get(i));
+		}
 	}
+	
+	public void keyPressed(KeyEvent e) {
+        System.out.println("keyPressed");
+    }
+
+    public void keyReleased(KeyEvent e) {
+		if(e.getKeyCode()==KeyEvent.VK_ENTER)
+			System.out.println("Enter key was pressed");
+	}
+    
+	 public String foundWords() {
+		 StringBuilder builder = new StringBuilder();
+		 for(Character ch : wordCheck) {
+			 builder.append(ch).append(',');
+			 System.out.println(builder);
+		 }
+		 if(builder.length() > 0){
+			 builder.deleteCharAt(builder.length() - 1); 
+		 }
+		 System.out.println(builder);
+		 return builder.toString(); 
+	 }
+	 
+	 Socket server;
+		public void connect(String address, int port) {
+			try {
+				server = new Socket(address, port);
+				System.out.println("Client connected");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	 
+		public void start() throws IOException {
+			if(server == null) {
+				return;
+			}
+			System.out.println("Listening for input");
+			try(Scanner si = new Scanner(System.in);
+					PrintWriter out = new PrintWriter(server.getOutputStream(), true);
+					BufferedReader in = new BufferedReader(new InputStreamReader(server.getInputStream()));){
+				String line = "";
+				while(true) {
+					try {
+						System.out.println("Waiting for input");
+						line = si.nextLine();
+						if(!"quit".equalsIgnoreCase(line)) {
+							out.println(line);
+						}
+						else {
+							break;
+						}
+						line = "";
+					}
+					catch(Exception e) {
+						System.out.println("Connection dropped");
+						break;
+					}
+				}
+				System.out.println("Exited loop");
+			}
+			catch(Exception e) {
+				e.printStackTrace();
+			}
+			finally {
+				close();
+			}
+		}
+		private void close() {
+			if(server != null) {
+				try {
+					server.close();
+					System.out.println("Closed socket");
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	
+	/*
+	 * public void foundWords() {
+	 * 
+	 * }
+	 */
 
 public static void main(String[] args) {
 	
 	String fileName = "gamedata.txt";
 	WordHunt game = new WordHunt();
+	//game.foundWords();
+	game.wordComparison();
 	if(game.fileFinder(fileName)) {
 		System.out.println("\n" + "Beginning game shortly.");
 		
@@ -73,6 +170,8 @@ public static void main(String[] args) {
 		container.setSize(new Dimension(800,800));
 		container.setMinimumSize(new Dimension(600, 600));
 		container.setBorder(BorderFactory.createLineBorder(Color.red));
+
+		//List<Location> wordCheck = new LinkedList<Location>();
 		
 		ArrayList<String> wordCheck = new ArrayList<String>();
 		
@@ -102,11 +201,12 @@ public static void main(String[] args) {
 					}
 				});
 		
-		
+
 		canvas.add(bt);
 		
 		i++;
-		    	}
+
+				}
 			}
 		frame.setVisible(true);
 		frame.add(canvas);
